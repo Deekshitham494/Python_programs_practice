f64b50438df75aeb9183340d73bb5d1d6ebf3742
unknown <deekshitham494@gmail.com>
2023-12-01 14:49:09 +0530

    01/12/2023

--- user_input.py
+++ user_input.py
@@ -4,3 +4,7 @@ print("the user input name is {}".format(str_x))
 num_a = int(input("Enter the value: "))
 print("the user input value is {}".format(num_a))
 print("the type of num a is {}".format(type(num_a)))
+
+tpl_a = (2, 5, 7, 8)
+tpl_b = (7, 8, 9, 2)
+# res = [9,13,16,10]
